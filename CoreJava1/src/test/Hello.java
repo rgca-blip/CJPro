00185deef68a4a6f3097261c4f8968db349bb1ab
rgsof <rgsof@MBIMG.Dlink>
2019-11-15 22:39:21 +0530

    change hello

--- CoreJava1/src/test/Hello.java
+++ CoreJava1/src/test/Hello.java
@@ -1,7 +1,65 @@
 package test;
 
 public class Hello {
-	public void m1() {
-		System.out.println("Hellow");
+	public String stdName;
+	public Integer stdNo;
+
+	public Hello(String stdName, Integer stdNo) {
+		super();
+		this.stdName = stdName;
+		this.stdNo = stdNo;
 	}
+
+	public String getStdName() {
+		return stdName;
+	}
+
+	public void setStdName(String stdName) {
+		this.stdName = stdName;
+	}
+
+	public Integer getStdNo() {
+		return stdNo;
+	}
+
+	public void setStdNo(Integer stdNo) {
+		this.stdNo = stdNo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((stdName == null) ? 0 : stdName.hashCode());
+		result = prime * result + ((stdNo == null) ? 0 : stdNo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Hello other = (Hello) obj;
+		if (stdName == null) {
+			if (other.stdName != null)
+				return false;
+		} else if (!stdName.equals(other.stdName))
+			return false;
+		if (stdNo == null) {
+			if (other.stdNo != null)
+				return false;
+		} else if (!stdNo.equals(other.stdNo))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Hello [stdName=" + stdName + ", stdNo=" + stdNo + "]";
+	}
+
 }
--- CoreJava1/src/test/HelloWorld.java
+++ CoreJava1/src/test/HelloWorld.java
@@ -1,11 +1,25 @@
 package test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		for(int i=1; i<=10; i++){
-		System.out.println("Hello World!");
+
+		Hello hl = new Hello("anil", 78);
+		Hello hl1 = new Hello("alekha", 66);
+		Hello hl2 = new Hello("amit", 23);
+		Hello hl3 = new Hello("anil", 78);
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("Hello World!");
 		}
+		Map<Object, String> hm = new HashMap<>();
+		hm.put(hl, "Asurabandha");
+		hm.put(hl1, "Gangapure");
+		hm.put(hl2, "Mayurabhanja");
+		hm.put(hl3, "Sorada");
+		System.out.println(hm.toString());
 	}
 
 }
